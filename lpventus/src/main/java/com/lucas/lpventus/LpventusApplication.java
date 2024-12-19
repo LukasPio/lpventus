1881b09026cb1e8b9883ca1952ffcf1865d5ec64
LukasPio <lucaspio.galvao@gmail.com>
2024-12-19 03:55:34 -0300

    initial commit

---
+++ lpventus/src/main/java/com/lucas/lpventus/LpventusApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.lpventus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpventusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpventusApplication.class, args);
+	}
+
+}
